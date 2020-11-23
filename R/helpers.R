020cdfa1c1672113cda3783167b16a01b893f787
Maximilian Held <info@maxheld.de>
2020-11-23 22:51:47 +0100

    reduce dependencies

--- R/helpers.R
+++ R/helpers.R
@@ -14,3 +14,15 @@ get_ghactions_url <- function(path = ".") {
   res$path <- do.call(paste, c(slug, "actions", sep = "/"))
   httr::build_url(res)
 }
+
+#' Determine if code is running inside GitHub Actions
+#'
+#' Looks for the `GITHUB_ACTIONS` environment variable, as [documented](https://docs.github.com/en/actions/configuring-and-managing-workflows/using-environment-variables)
+#'
+#' @keywords internal
+#'
+#' @export
+# duplicate from muggle, but better than importing all of muggle
+is_github_actions <- function() {
+  Sys.getenv("GITHUB_ACTIONS") == "true"
+}
--- tests/testthat/test-account.R
+++ tests/testthat/test-account.R
@@ -1,5 +1,5 @@
 test_that("az account works", {
-  skip_if_not(shinycaas::is_github_actions() | Sys.getenv("LOGNAME") == "max")
+  skip_if_not(is_github_actions() | Sys.getenv("LOGNAME") == "max")
   local_az_account()
   # only default subscription matters here
   expect_equal(az_account_show()$name, "subugoe")
