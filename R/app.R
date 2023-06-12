289e1125a021b37b427ad1aebe5997ec0e663dae
Maximilian Held <info@maxheld.de>
2023-06-12 17:28:37 +0200

    add source info to app

--- R/app.R
+++ R/app.R
@@ -8,7 +8,7 @@
 serve_all <- function() {
   host <- Sys.getenv("R_HOST", "127.0.0.1")
   port_shiny <- get_port_safely(3838L, host = host)
-  shiny::runExample("01_hello", host = host, port = port_shiny)
+  shiny::runApp(app(), host = host, port = port_shiny)
 }
 
 #' Helper to get a safe port
@@ -29,3 +29,100 @@ get_port_safely <- function(preferred, host) {
 }
 
 test_fun <- function() AzureRMR::is_url()
+
+#' Test app
+#' @noRd
+app <- function() {
+  shiny::shinyApp(
+    ui = bslib::page(nav_menu_source()),
+    server = function(input, output, session) TRUE
+  )
+}
+
+#' Build info
+#' Useful to print inside shiny app.
+#'
+#' @details
+#' This should be evaluated only when the app is built.
+#' @noRd
+show_build_info <- function() {
+  version <- utils::packageVersion("azureappservice")
+  # appease linter, check
+  version
+  is_in_docker <- Sys.getenv("GITHUB_REF_NAME") != ""
+  version <- glue::glue("version {version}")
+  scm <- {
+    if (is_in_docker) {
+      glue::glue_collapse(
+        c(
+          glue::glue("from git sha {Sys.getenv('GITHUB_SHA')},"),
+          glue::glue("on git branch {Sys.getenv('GITHUB_REF_NAME')}")
+        )
+      )
+    } else {
+      glue::glue("local")
+    }
+  }
+  build_time <- lubridate::format_ISO8601(
+    build_time,
+    usetz = TRUE,
+    precision = "ymdhm"
+  )
+  launched <- lubridate::format_ISO8601(
+    run_time(),
+    usetz = TRUE,
+    precision = "ymdhm"
+  )
+  built <- glue::glue("built at {build_time}")
+  launched <- glue::glue("launched at {launched}")
+  c(version = version, scm = scm, built = built, launched = launched)
+}
+
+#' Store build time in package
+#' @noRd
+build_time <- lubridate::now(tz = "Europe/Berlin")
+
+#' Get run time
+#' @noRd
+run_time <- function() lubridate::now(tz = "Europe/Berlin")
+
+#' Source info
+#' @noRd
+nav_menu_source <- function() {
+  all <- show_build_info()
+  bslib::nav_menu(
+    title = "Source",
+    bslib::nav_item(
+      shiny::tags$a(
+        shiny::tags$i(bsicons::bs_icon("github")),
+        "Repository",
+        href = "https://github.com/dataheld/azureappservice"
+      )
+    ),
+    bslib::nav_item(
+      shiny::tags$a(
+        shiny::tags$i(bsicons::bs_icon("git")), all["scm"]
+      )
+    ),
+    bslib::nav_item(
+      shiny::tags$a(
+        shiny::tags$i(bsicons::bs_icon("box")), all["built"]
+      )
+    ),
+    bslib::nav_item(
+      shiny::tags$a(
+        shiny::tags$i(bsicons::bs_icon("play")), all["launched"]
+      )
+    ),
+    icon = shiny::tags$i(bsicons::bs_icon("code")),
+    align = "right"
+  )
+}
+
+#' Link to gh repo
+#' @noRd
+link_gh <- shiny::tags$a(
+  shiny::tags$i(bsicons::bs_icon("github")),
+  "Repository",
+  href = "https://github.com/dataheld/azureappservice"
+)
